f125a9d3dc0ed9e5a17dfc414c9d48e39fd5f7af
Tagliente-Simone <simone.tagliente@mail.polimi.it>
2023-03-21 17:41:06 +0100

    some files deleted

--- 2.0/ResultPrinter.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def create_result_csv(origine_x, origine_y, rotazione):
-
-
-    with open('result.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["origine_x", "origine_y", "rotazione"])
-        writer.writerow([origine_x, origine_y, rotazione])
